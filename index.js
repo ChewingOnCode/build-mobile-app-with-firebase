72b67989b583ab8c4ea20c3bd48e31b68245c97b
Joseph Chu <karkinoscode@gmail.com>
2024-04-27 07:07:25 -0400

    Added js functionality for adding items to application and database

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const input = document.getElementById("input-field");
-const addBtn = document.getElementById("add-button");
+// import initialize App from firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {
+  getDatabase,
+  push,
+  ref,
+} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 
-addBtn.addEventListener("click",function(){
-    let inputValue = input.value;
-    console.log(inputValue);
-} )
+const appSettings = {
+  databaseURL: "https://shopping-list-379b3-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDb = ref(database, "shoppingList");
+
+const inputEl = document.getElementById("input-field");
+const addBtn = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
+
+addBtn.addEventListener("click", function () {
+  let inputValue = inputEl.value;
+  push(shoppingListInDb, inputValue);
+  clearInputFieldEl();
+  appendItemToShoppingListEl(inputValue);
+  // console.log(inputValue)
+});
+
+function clearInputFieldEl() {
+  // clear the input on the button being clicked
+  inputEl.value = "";
+}
+
+function appendItemToShoppingListEl(itemValue) {
+  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
