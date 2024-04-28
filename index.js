6b59bcb5fe55b3c7c88ecb7a46aac418fa3afc12
Joseph Chu <karkinoscode@gmail.com>
2024-04-28 04:11:37 -0400

    updated js functionality and added Flexbox to application. Also fixed .gitignore

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import {
   getDatabase,
   push,
   ref,
+  onValue,
+  remove
 } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 
 const appSettings = {
-  databaseURL: "https://shopping-list-379b3-default-rtdb.firebaseio.com/",
+  databaseURL: "https://shopping-list-379b3-default-rtdb.firebaseio.com/"
 };
 
 const app = initializeApp(appSettings);
@@ -22,15 +24,48 @@ addBtn.addEventListener("click", function () {
   let inputValue = inputEl.value;
   push(shoppingListInDb, inputValue);
   clearInputFieldEl();
-  appendItemToShoppingListEl(inputValue);
   // console.log(inputValue)
+});
+
+onValue(shoppingListInDb, function (snapshot) {
+  // console.log to test functionality
+  //   console.log(snapshot.val);
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
+
+    clearShoppingListEl();
+    // For each item in the array, append it to the shoppingListEl
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemId = currentItem[0];
+      let currentItemValue = currentItem[1];
+      appendItemToShoppingListEl(currentItem);
+    }
+  } else {
+    shoppingListEl.innerHTML = `<li>No items in the list</li>`;
+  }
 });
 
 function clearInputFieldEl() {
   // clear the input on the button being clicked
   inputEl.value = "";
 }
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
+}
 
-function appendItemToShoppingListEl(itemValue) {
-  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+function appendItemToShoppingListEl(item) {
+  // shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+  let itemID = item[0];
+  let itemValue = item[1];
+  // create a new li element
+  let newEl = document.createElement("li");
+  newEl.textContent = itemValue;
+  // On click, delete the item from the database
+  newEl.addEventListener("click", function () {
+    let exactLocationInDb = ref(database, `shoppingList/${itemID}`);
+    // Delete the item from the database
+    remove(exactLocationInDb);
+  });
+  shoppingListEl.append(newEl);
 }
